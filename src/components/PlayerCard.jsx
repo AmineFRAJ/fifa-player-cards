926b36c629de730cdddbe5024192a3a6208c6fb1
AmineFRAJ <amine0924@gmail.com>
2024-08-05 11:37:28 +0100

    add destruction

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,32 +1,32 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import '../styles/card.css'
+ 
 
-const PlayerCard = ({ player }) => {
+const PlayerCard = ({ imageUrl,name,team,age,nationality,jerseyNumber }) => {
   return (
     <div>
     <Card className="custom-card">
-      <Card.Img variant="top" src={player.imageUrl} className="custom-card-img" /> <hr/>
+      <Card.Img variant="top" src={imageUrl} className="custom-card-img" /> <hr/>
       <Card.Body className="custom-card-body">
-        <Card.Title className="custom-card-title">{player.name}</Card.Title>
-        <Card.Text className="custom-card-text">{`Team : ${player.team}`}</Card.Text>
-        <Card.Text className="custom-card-text">{`Nationality : ${player.nationality}`}</Card.Text>
-        <Card.Text className="custom-card-text">{`Age : ${player.age}`}</Card.Text>
-        <Card.Text className="custom-card-text">{`Number : ${player.jerseyNumber}`}</Card.Text>
+        <Card.Title className="custom-card-title">{name}</Card.Title>
+        <Card.Text className="custom-card-text">{`Team : ${team}`}</Card.Text>
+        <Card.Text className="custom-card-text">{`Nationality : ${nationality}`}</Card.Text>
+        <Card.Text className="custom-card-text">{`Age : ${age}`}</Card.Text>
+        <Card.Text className="custom-card-text">{`Number : ${jerseyNumber}`}</Card.Text>
       </Card.Body>
     </Card>
   </div>
   );
 };
+
+
+
 PlayerCard.defaultProps = {
-  player: {
     imageUrl: "defaultImageUrl.jpg",
     name: "Default Name",
     team: "Default Team",
     nationality: "Default Nationality",
     age: "Default Age",
-    jerseyNumber: "Default Number"
-  }
-};
-
+    jerseyNumber: "Default Number"}
 export default PlayerCard;
--- src/components/PlayersList.jsx
+++ src/components/PlayersList.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import players from "../players";
 import PlayerCard from "./PlayerCard";
- 
+console.log(players);
 const PlayersList = () => {
-  console.log(players);
+ 
   return (
   <div style={{display:'flex' ,justifyContent:'space-around', gap:"20px", flexWrap:"wrap",marginTop:"50px"}}>
     {
-players.map((player)=> <PlayerCard player={player} key={player.name}/>)
+players.map((player)=> <PlayerCard {...player} key={player.name}/>)
     }
   </div>
     )
